580b1581fdb0ed53299b0bd4a675d7ec3a8cdefb
rahulpatel2151 <rahulpatel2151@gmail.com>
2020-11-27 15:23:19 +0530

    SCAssignment1 inital commit

---
+++ SourceControlAssignment1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SourceControlAssignment1.Models;
+namespace SourceControlAssignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegistrationData(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.result = "Data complete";
+            }
+            else
+            {
+                ViewBag.result = "Data incomplete";
+
+            }
+            return View("Index");
+        }
+
+
+    }
+}
---
+++ SourceControlAssignment1/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SourceControlAssignment1.Models
+{
+    public class Employee
+    {
+        [Required]
+        [DisplayName("FirstName")]
+        public String firstName { get; set; }
+        [Required]
+        [DisplayName("LastName")]
+        public String lastName { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [DisplayName("MobileNo.")]
+        public String mobileNo { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email")]
+        public String email { get; set; }
+        [Required]
+        [Range(1,130)]
+        [DisplayName("Age")]
+        public int age { get; set; }
+        [Required]
+        [MaxLength(500)]
+        [DisplayName("Address")]
+        public String address { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Birthdate")]
+        public DateTime birthDate { get; set; }
+        [Required]
+        [DisplayName("Profile Photo")]
+        [FileExtensions(Extensions =".jpg .jpeg .png")]
+        public HttpPostedFileBase profilePhoto { get; set; }
+    }
+}
